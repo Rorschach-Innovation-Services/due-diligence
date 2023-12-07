fd85e5c46c37817718c0abe2f386d6e7922dd5cc
Talifhani Mulaudzi <mldtal001@myuct.ac.za>
2023-12-07 20:37:18 +0200

    test

--- app/api/answer/route.ts
+++ app/api/answer/route.ts
@@ -1,89 +1,86 @@
-import { CategoryModel } from "@/models/category";
+import {CategoryModel} from "@/models/category";
 import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest } from "next/server";
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    if (req) {
 
-      const { searchParams } = new URL(req.url as string);
-      const categoryId = searchParams.get('categoryId');
-      const questionId = searchParams.get('questionId');
-      const lastedited = searchParams.get('lastedited');
-      //   console.log("Cat ID:", searchParams)
-
+export async function POST(req: NextRequest | NextApiRequest, res: NextApiResponse) {
+    try {
+        const { searchParams } = new URL(req.url as string);
+        const categoryId = searchParams.get('categoryId');
+        const questionId = searchParams.get('questionId');
+        const lastedited = searchParams.get('lastedited');
+    //   console.log("Cat ID:", searchParams)
+  
       // Ensure categoryId is provided
       if (!categoryId) {
         return Response.json({ error: 'Category ID is required' });
       }
-
+  
       // Find the category by ID
       const category = await CategoryModel.findOne({ _id: categoryId });
-
+  
       // Check if the category is not found
       if (!category) {
         return Response.json({ error: 'Category not found' });
       }
-
+  
       // Create a new question with empty title and content
-      //   const newQuestion = {
-      //     id: "none", // Assume you have a function to generate a unique ID
-      //     title: '',
-      //     contents: [],
-      //     lastedited: new Date().toISOString(),
-      //   };
+    //   const newQuestion = {
+    //     id: "none", // Assume you have a function to generate a unique ID
+    //     title: '',
+    //     contents: [],
+    //     lastedited: new Date().toISOString(),
+    //   };
 
-      let question
-      if (lastedited) {
+    let question
+    if (lastedited) {
 
         question = category.questions.find((q: {
           lastedited: string; _id: string | string[];
         }) => q.lastedited == lastedited);
-
+  
         console.log("resp", question)
       } else {
         console.log("Cate....", questionId)
         question = category.questions.find((q: { _id: string | string[]; }) => q._id == questionId);
       }
 
-
-
+  
+  
       // Add the new question to the category
       question.contents.push("");
-
-      if (!category.group) {
+  
+      if(!category.group) {
         category.group = null;
       }
-
+      
       const updatedCategory = await category.save();
-
+  
       const questions = await fetchQuestions(categoryId);
-
+  
       console.log("New Question:", question)
-
+  
       return Response.json({ updatedCategory });
-
+    } catch (error) {
+      console.error('Error creating question:', error);
+      return Response.json({ error: 'Internal Server Error' });
     }
-  } catch (error) {
-    console.error('Error creating question:', error);
-    return Response.json({ error: 'Internal Server Error' });
   }
-}
+  
+  const fetchQuestions = async (categoryId: string) => {
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/categories?id=${categoryId}`);
+      const data = await response.json();
+      return data?.category?.questions || [];
+    } catch (error) {
+      console.error("Error fetching questions:", error);
+      return [];
+    }
+  };
 
-const fetchQuestions = async (categoryId: string) => {
+export async function DELETE(req: NextRequest | NextApiRequest, res: NextApiResponse) {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/categories?id=${categoryId}`);
-    const data = await response.json();
-    return data?.category?.questions || [];
-  } catch (error) {
-    console.error("Error fetching questions:", error);
-    return [];
-  }
-};
-
-export async function DELETE(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    if (req) {
-      const { searchParams } = new URL(req.url as string);
+    const { searchParams } = new URL(req.url as string);
     const categoryId = searchParams.get('categoryId');
     const questionId = searchParams.get('questionId');
     const contentIndex = searchParams.get('contentIndex');
@@ -112,7 +109,7 @@ export async function DELETE(req: NextApiRequest, res: NextApiResponse) {
       question = category.questions.find((q: { lastedited: string; _id: string | string[] }) => q.lastedited === lastedited);
     } else {
       question = category.questions.find((q: { _id: string | string[] }) => q._id == questionId);
-      //   console.log("No lastedited!")
+    //   console.log("No lastedited!")
     }
 
     // console.log(question)
@@ -132,14 +129,13 @@ export async function DELETE(req: NextApiRequest, res: NextApiResponse) {
     // Remove the content item from the question
     question.contents.splice(contentIndex, 1);
 
-    if (!category.group) {
+    if(!category.group) {
       category.group = null;
     }
-
+    
     const updatedCategory = await category.save();
 
     return Response.json({ updatedCategory });
-    }
   } catch (error) {
     console.error('Error deleting question content:', error);
     return Response.json({ error: 'Internal Server Error' });
