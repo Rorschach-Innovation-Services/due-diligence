d9e9c7e98e4e8e9e662b6f2650d7ac15c896a0aa
Talifhani Mulaudzi <mldtal001@myuct.ac.za>
2023-12-11 14:44:06 +0200

    maxNumberOfWorkers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    experimental: {
+      maxNumberOfWorkers: 2
+    }  
+  }
+  
+  module.exports = nextConfig
