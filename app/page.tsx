719a78ef86f0157bfc7a7d9518254a28f978a135
Talifhani Mulaudzi <mldtal001@myuct.ac.za>
2023-12-11 15:39:09 +0200

    lazy loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import Layout from '../components/Layout'
+// import Layout from '../components/Layout'
+import dynamic from "next/dynamic";
 
+const Layout = dynamic(() => import("../components/Layout"), {
+  ssr: false,
+});
 export default function Home() {
   return (
     <Layout >
